f3d4552b54dff5f84148caa30528b76ad8e3b3c6
mael_ <mael.jozi@gmail.com>
2024-03-08 01:40:56 -0300

    feat: add follower role on postResour

--- src/main/java/io/github/maeloliveira/rest/PostResource.java
+++ src/main/java/io/github/maeloliveira/rest/PostResource.java
@@ -2,6 +2,7 @@ package io.github.maeloliveira.rest;
 
 import io.github.maeloliveira.domain.model.Post;
 import io.github.maeloliveira.domain.model.User;
+import io.github.maeloliveira.domain.repository.FollowerRepository;
 import io.github.maeloliveira.domain.repository.PostRepository;
 import io.github.maeloliveira.domain.repository.UserRepository;
 import io.github.maeloliveira.rest.dto.CreatePostRequest;
@@ -21,11 +22,13 @@ public class PostResource {
 
     private final UserRepository userRepository;
     private final PostRepository repository;
+    private final FollowerRepository followerRepository;
 
     @Inject
-    public PostResource(UserRepository userRepository, PostRepository repository) {
+    public PostResource(UserRepository userRepository, PostRepository repository, FollowerRepository followerRepository) {
         this.userRepository = userRepository;
         this.repository = repository;
+        this.followerRepository = followerRepository;
     }
 
     @POST
@@ -45,13 +48,36 @@ public class PostResource {
     }
 
     @GET
-    public Response listPosts(@PathParam("userId") Long userId) {
+    public Response listPosts(@PathParam("userId") Long userId,
+                              @HeaderParam("followerId") Long followerId) {
         User user = userRepository.findById(userId);
         if (user == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+        if (followerId == null) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("You forgot the header followerId")
+                    .build();
+        }
+
+        User follower = userRepository.findById(followerId);
+
+        if(follower == null){
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Inexistent followerId")
+                    .build();
+        }
+
+        boolean follows = followerRepository.follows(follower, user);
+        if (!follows) {
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("You cant't see these posts")
+                    .build();
+        }
+
         var query = repository.find(
-                "user", Sort.by("dateTime", Sort.Direction.Descending) , user);
+                "user", Sort.by("dateTime", Sort.Direction.Descending), user);
         var list = query.list();
 
         var postResponseList = list.stream().map(PostResponse::fromEntity)
